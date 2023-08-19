db5f9c6226b95cf40d77e5f21df77713d968250e
SumonRahman24 <mustafizurrahmansumon24@gmail.com>
2023-08-19 12:59:20 +0600

    ok

--- index2.js
+++ index2.js
@@ -1,38 +1,56 @@
-function calculateAndDisplay(input1Id, input2Id, displayId, operation) {
-  const input1 = parseFloat(document.getElementById(input1Id).value);
-  const input2 = parseFloat(document.getElementById(input2Id).value);
-
-  const result = operation(input1, input2);
-
-  document.getElementById(displayId).innerText = result;
-  document.getElementById(input1Id).value = "";
-  document.getElementById(input2Id).value = "";
+function getFloatValue(input) {
+  return parseFloat(document.getElementById(input).value);
 }
 
-function calculateTriangleArea(base, height) {
-  return 0.5 * base * height;
+function getClearValue(inputId1, inputId2) {
+  document.getElementById(inputId1).value = "";
+  document.getElementById(inputId2).value = "";
 }
 
-function calculateRectangleArea(length, width) {
-  return length * width;
+function calculationOperation(num, inputId1, inputId2) {
+  const input1 = getFloatValue(inputId1);
+  const input2 = getFloatValue(inputId2);
+
+  const result = num * input1 * input2;
+  getClearValue(inputId1, inputId2);
+  return result;
 }
 
-const triangleBtn = document.getElementById("triangle-btn");
-triangleBtn.addEventListener("click", function () {
-  calculateAndDisplay(
+function displayArea(name, result) {
+  const areaContainer = document.getElementById("area-container");
+  let count = areaContainer.childElementCount;
+
+  const newPara = document.createElement("div");
+  newPara.innerHTML = `${
+    count + 1
+  }. ${name} ${result} cm<sup>2</sup> <button class="bg-[#1090D8] text-white font-semibold  rounded-md">convert to m<sup>2</sup></button>`;
+  areaContainer.appendChild(newPara);
+}
+
+function triangleBtn() {
+  const result = calculationOperation(
+    0.5,
     "triangle-input1",
-    "triangle-input2",
-    "triangle-display",
-    calculateTriangleArea
+    "triangle-input2"
   );
-});
+  displayArea("Triangle", result);
+}
 
-const rectangleBtn = document.getElementById("rectangle-btn");
-rectangleBtn.addEventListener("click", function () {
-  calculateAndDisplay(
+function rectangleBtn() {
+  const result = calculationOperation(
+    1,
     "rectangle-input1",
-    "rectangle-input2",
-    "rectangle-display",
-    calculateRectangleArea
+    "rectangle-input2"
   );
-});
+  displayArea("rectangle", result);
+}
+
+function parallelogramBtn() {
+  console.log("btn");
+  const result = calculationOperation(
+    0.5,
+    "parallelogram-input1",
+    "parallelogram-input2"
+  );
+  displayArea("parallelogram", result);
+}
